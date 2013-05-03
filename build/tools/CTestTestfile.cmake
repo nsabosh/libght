803702864021dd60a95f587ffafb0f3488595787
N Sabo, RNCan <nsabo@nrcan.gc.ca>
2013-05-03 11:38:05 -0400

    Changes to allow building on Windows

---
+++ build/tools/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: Q:/GitHub/libght/tools
+# Build directory: Q:/GitHub/libght/build/tools
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
